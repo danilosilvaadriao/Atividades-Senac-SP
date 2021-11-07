9cefdb361f546a0a81cedddcdb67c2e703b5f8df
Danilo Silva Adriao <82722083+danilosilvaadriao@users.noreply.github.com>
2021-11-07 20:00:12 -0300

    add script.js
    add archive script.js

---
+++ Algoritmos-Aula1/script.js
@@ -0,0 +1,21 @@
+// Aula de algoritmo 1 - Senac
+
+// entrada de dados
+
+var nomeRemetente = prompt("Qual o nome do remetente? ");
+
+var nomeDestinatario = prompt("Qual o nome do destinatario? ");
+
+var mensagem = prompt("Qual a mensagem? ");
+
+// saida de dados
+
+console.log("");
+
+console.log("Remetente: " + nomeRemetente);
+
+console.log("Destinatario: " + nomeDestinatario);
+
+console.log("");
+
+console.log("Mensagem: " + mensagem);
